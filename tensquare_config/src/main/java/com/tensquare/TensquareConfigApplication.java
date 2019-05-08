4b97f142335d376390b2ad2cab9ad295f5f993ae
guancgsuccess <849962874@qq.com>
2019-05-08 10:40:54 +0800

    持续集成测试

---
+++ tensquare_config/src/main/java/com/tensquare/TensquareConfigApplication.java
@@ -0,0 +1,15 @@
+package com.tensquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer //开启配置服务
+@SpringBootApplication
+public class TensquareConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensquareConfigApplication.class, args);
+	}
+
+}
